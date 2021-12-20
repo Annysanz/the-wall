2956e1d429246be19b15eb88151bfe3a623d7d4b
Annysanz <annysanz17@gmail.com>
2021-12-20 12:04:42 +0100

    starting project

---
+++ src/app/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div className='app-wrapper d-flex flex-column justify-content-between' id='LiliApp-container'>
+    </div>
+  );
+};
+
+export default App;
